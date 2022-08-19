4b6c8bfaa068264f99c9342214c41d5bdc88c984
newstarellie <newstarellie@gmail.com>
2022-08-19 23:12:52 +0800

    基本功能

---
+++ src/components/TODO-table.vue
@@ -0,0 +1,45 @@
+<script>
+import TODOInput from './TODO-input.vue';
+import TODOTaskItem from './TODO-taskItem.vue';
+export default {
+  components: { TODOInput, TODOTaskItem },
+  data() {
+    return {
+      taskArr: [],
+    };
+  },
+  methods: {
+    getTask: function (task) {
+      this.taskArr.push(task);
+    },
+    deleteAllTask() {
+      this.taskArr = [];
+    },
+  },
+};
+</script>
+
+<template>
+  <h2>TODO</h2>
+  <h2>{{ taskArr }}</h2>
+  <TODOInput @sendOut="getTask" @deleteAllTask="deleteAllTask" />
+  <ul>
+    <TODOTaskItem
+      v-for="(item, index) in taskArr"
+      :key="index"
+      :taskName="item"
+    >
+    </TODOTaskItem>
+  </ul>
+</template>
+
+<style scoped>
+h2 {
+  color: red;
+  font-size: 1.2rem;
+}
+span {
+  border: 1px solid black;
+  margin: 2px;
+}
+</style>
---
+++ src/components/TODO-taskItem.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  props: ['taskName'],
+  data() {
+    return {
+      message: 1,
+      deleted: false,
+    };
+  },
+  methods: {
+    deleteItem: function () {
+      this.deleted = true;
+    },
+  },
+  computed: {},
+};
+</script>
+
+<template>
+  <li v-if="!deleted">
+    {{ taskName }}
+    <span @click="deleteItem()">X</span>
+  </li>
+</template>
+
+<style scoped>
+h2 {
+  color: red;
+  font-size: 1.2rem;
+}
+span {
+  border: 1px solid black;
+  padding: 0 5px;
+  margin: 2px;
+}
+li {
+  position: relative;
+  width: 150px;
+}
+li > span {
+  position: absolute;
+  right: 0;
+}
+</style>
