ccf48a130adeab9856a0a6e2fdb67813a9bea845
newstarellie <newstarellie@gmail.com>
2022-08-20 11:04:17 +0800

    做好'完成'功能

---
+++ .history/src/components/TODO-input_20220819231549.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  data() {
+    return {
+      task: '',
+    };
+  },
+  methods: {
+    sendOutTask() {
+      this.$emit('sendOut', this.task);
+      this.task = '';
+    },
+    deleteAllTask() {
+      this.$emit('deleteAllTask', '');
+      console.log('完成');
+    },
+  },
+  components: { TODOTaskItem },
+  computed: {},
+};
+</script>
+
+<template>
+  <h2>{{ task }}</h2>
+  <input
+    type="text"
+    placeholder="輸入要做的事情"
+    v-model="task"
+    v-on:keyup.enter="sendOutTask(this.task)"
+  />
+  <span v-on:click="sendOutTask(this.task)">送出</span>
+  <span @click="deleteAllTask()">刪除全部</span>
+</template>
+
+<style scoped>
+h2 {
+  color: red;
+  font-size: 1.2rem;
+}
+span {
+  border: 1px solid black;
+  margin: 2px;
+}
+</style>
